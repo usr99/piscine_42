05ef559754f7d1c176c69885dd91b989619440e2
Mael MARTIN <mamartin@e3r6p4.42.fr>
2020-08-17 05:35:19 +0200

    c10

---
+++ ex01/ft_read_file.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_file.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/17 05:32:04 by mamartin          #+#    #+#             */
+/*   Updated: 2020/08/17 05:32:07 by mamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_READ_FILE_H
+# define FT_READ_FILE_H
+
+int		ft_strlen(char *str);
+void	ft_putstr(int fd, char *str);
+void	ft_read_file(int fd, char *str);
+
+#endif
---
+++ ex01/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/17 05:16:33 by mamartin          #+#    #+#             */
+/*   Updated: 2020/08/17 05:33:28 by mamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <libgen.h>
+#include "ft_read_file.h"
+
+int	main(int argc, char **argv)
+{
+
+	return (0);
+}
